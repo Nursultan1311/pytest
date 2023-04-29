e348923d6d067463f3be09e8666d1faf3fddd760
Nursultan1311 <92705319+Nursultan1311@users.noreply.github.com>
2023-04-29 23:50:57 +0600

    Update test_script.py

--- test_script.py
+++ test_script.py
@@ -1,1 +1,12 @@
+import unittest
+
+class TestAddition(unittest.TestCase):
+
+    def test_addition(self):
+        result = 2 + 3
+        expected = 5
+        self.assertEqual(result, expected)
+
+if __name__ == '__main__':
+    unittest.main()
 
